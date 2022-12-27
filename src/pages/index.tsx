b89d7998ddc540cc9077ea7ea0c0f4a5050d671e
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-12-27 17:30:06 -0300

    switchingo to TS

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import { Home } from '../templates/Home';
+import { loadPages } from '../api/load-pages';
+import { GetStaticProps } from 'next';
+
+export type IndexProps = {
+  data: [];
+};
+
+export default function Index({ data = null }: IndexProps) {
+  return <Home data={data} />;
+}
+
+export const getStaticProps: GetStaticProps<IndexProps> = async () => {
+  let data = null;
+
+  try {
+    data = await loadPages('landing-page');
+  } catch (e) {
+    //
+  }
+
+  if (!data || !data.length) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
