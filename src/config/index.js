3e665583387b1ba32b98cc89dd7f24afb23ef646
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-12-28 14:08:06 -0300

    config and styles to TS

--- src/config/index.js
+++
@@ -1,5 +0,0 @@
-export default {
-  url: 'http://127.0.0.1:1337',
-  defaultSlug: 'landing-page',
-  siteName: 'Samuel Carvalho',
-};
---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export default {
+  url: "http://127.0.0.1:1337",
+  defaultSlug: "landing-page",
+  siteName: "Samuel Carvalho",
+};
